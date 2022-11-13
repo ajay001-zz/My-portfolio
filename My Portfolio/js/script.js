16ca7473c41a88c27b862d109c68d5b99b0d4baa
azay <81252778+ajay001-zz@users.noreply.github.com>
2022-11-13 07:05:22 +0545

    Add files via upload

---
+++ My Portfolio/js/script.js
@@ -0,0 +1,7 @@
+/**==========================typing animation================== */
+var typed = new Typed(".typing", {
+    strings: [" ", "Learning", "Web Designing", "Web Developing", "Photography", "and many more"],
+    typeSpeed: 100,
+    BackSpeed: 60,
+    loop: true
+})
